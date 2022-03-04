2a19381287a2c70bdde9eb614f20f93fcbb8173b
Omar Atef <20190356@stud.fci-cu.edu.eg>
2022-03-04 18:58:41 +0200

    test

---
+++ LeetCode/RevereseString.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+public class RevereseString {
+    public RevereseString(){}
+    public static void reverseString(char[] s) {
+        StringBuilder temp=new StringBuilder();
+        for (int i = s.length-1 ; i >= 0;i--){
+            char x = s[i];
+            temp.append(x);
+        }
+
+        for (int i =0 ;i >= temp.length()-1;i++){
+            s[i]= temp.charAt(i);
+        }
+
+    }
+
+    public static String reverseWords(String s) {
+
+        char [] ans = s.toCharArray();
+
+        reverseString(ans);
+
+        System.out.println(ans);
+        return s;
+    }
+
+    public static void main(String[] args) {
+        String lol = "ABCDEFU";
+        RevereseString.reverseWords(lol);
+
+    }
+}
---
+++ LeetCode/twoGram.java
@@ -0,0 +1,63 @@
+package LeetCode;
+
+import javax.swing.plaf.IconUIResource;
+import java.util.*;
+
+public class twoGram {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num;
+        num = in.nextInt();
+        String[] arr = new String[num];
+        for (int i = 0; i < num; i++) {
+            char c = '\0';
+            c = (in.next().charAt(0));
+            arr[i] = Character.toString(c);
+        }
+//        for (int i = 0; i < num; i++) {
+//            System.out.print(arr[i]);
+//        }
+//        System.out.println();
+
+        String[] arr2 = new String[num - 1];
+
+        for (int i = 0; i < num; i++) {
+            if (i == num - 1)
+                break;
+            String two = arr[i] + arr[i + 1];
+            arr2[i] = two;
+        }
+//
+//        for (int i = 0; i < num - 1; i++) {
+//            System.out.println(arr2[i]);
+//        }
+        Hashtable<String, Integer> Grams = new Hashtable<>();
+        for (int i = 0; i < num - 1; i++) {
+            String g = arr2[i];
+            int flag = 0;
+            for (int j = i; j < num - 1; j++) {
+                if (g.equals(arr2[j])) {
+                    flag++;
+                }
+            }
+            if (Grams.containsKey(g)) {
+                continue;
+            }
+            Grams.put(arr2[i], flag);
+
+        }
+//        System.out.println("\nHashMap Values");
+//        for (String val : Grams.keySet()) {
+//            System.out.println(val + " " + Grams.get(val));
+//        }
+        int max = Collections.max(Grams.values());
+        for (String val : Grams.keySet()) {
+            if (Grams.get(val) == max) {
+                System.out.println(val);
+                break;
+            }
+        }
+
+    }
+}
